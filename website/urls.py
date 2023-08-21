10194f5ac77b638d5eca3890725c678b819f1cea
MichalCiesiolka <michal.ciesiolka3@o2.pl>
2023-08-21 12:28:20 +0200

    Registration and minor updates

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('finished/', views.finished_tasks, name='finished_tasks'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.list_element, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('finish_record/<int:pk>', views.mark_as_finished, name='finish_record'),
     path('restore_record/<int:pk>', views.mark_as_unfinished, name='restore_record'),
     path('finished_record/<int:pk>', views.finished_element, name='finished_record'),
+    path('update_finished_record/<int:pk>', views.update_finished_record, name='update_finished_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Record
-from .forms import AddTaskForm
+from .forms import SignUpForm, AddTaskForm
 from django import forms
 import datetime
 
@@ -28,21 +28,29 @@ def home(request):
         return render(request, 'home.html', {'records': records})
 
 
+# def finished_tasks(request):
+#     records = Record.objects.all()
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             messages.success(request, "Logged In")
+#             return redirect('home')
+#         else:
+#             messages.success(request, "An error occurred")
+#             return redirect('home')
+#     else:
+#         return render(request, 'finished_tasks.html', {'records': records})
+
 def finished_tasks(request):
     records = Record.objects.all()
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Logged In")
-            return redirect('home')
-        else:
-            messages.success(request, "An error occurred")
-            return redirect('home')
-    else:
+    if request.user.is_authenticated:
         return render(request, 'finished_tasks.html', {'records': records})
+    else:
+        messages.success(request, "You must be logged in to view this page")
+        return redirect('home')
 
 
 def login_user(request):
@@ -51,8 +59,25 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success(request, "You have benn logged out")
+    messages.success(request, "You have been logged out")
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 
 
 def list_element(request, pk):
@@ -148,3 +173,17 @@ def update_record(request, pk):
     else:
         messages.success(request, "You must be logged in to update task")
         return redirect('home')
+
+
+def update_finished_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddTaskForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated")
+            return redirect('finished_tasks')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in to update task")
+        return redirect('finished_tasks')
